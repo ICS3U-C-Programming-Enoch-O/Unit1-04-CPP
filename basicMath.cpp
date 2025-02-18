3c2ee46363de5192d2d826f32f4b5b05eb02ac41
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-02-18 18:05:35 +0000

    Added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Created by: Enoch
+// Created on: Apr. 26, 2025
+// Copyright (c) 2025 All rights reserved.
+// This program does basic math.
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "9 + 2 = " << 9 + 2 << std::endl;
+    std::cout << "7 - 3 = " << 7 - 3 << std::endl;
+    std::cout << "10 / 2 = " << 10 / 2 << std::endl;
+    std::cout << "-4 / 2 = " << -4 / 2 << std::endl;
+    std::cout << "2 * 3 = " << 2 * 3 << std::endl;
+    std::cout << "pow(2, 3) = " << pow(2, 3) << std::endl;
+    std::cout << "1 / 3 = " << 1.0 / 3.0 << std::endl;
+}
